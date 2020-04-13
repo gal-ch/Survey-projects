8df01b8454bb3e08d78528b888d5c62d93149608
gal hadad <galchadad@gmail.com>
2020-04-13 22:26:57 +0300

    fix

--- matches/utils.py
+++ matches/utils.py
@@ -7,7 +7,7 @@ users = User.objects.all()
 all_user_answers = UserAnswer.objects.all().order_by("user__id")
 
 
-def get_points(user_a, user_b):
+def get_users_match(user_a, user_b):
     q1 = Q(useranswer__user=user_a)
     q2 = Q(useranswer__user=user_b)
     question_set = Question.objects.filter(q1 | q2).distinct()
@@ -24,7 +24,7 @@ def get_points(user_a, user_b):
         try:
             b = UserAnswer.objects.get(user=user_b, question=question)
         except:
-            b_answer = None
+            b = None
         if a and b:
             question_in_common += 1
             if a.other_user_answer == b.user_answer:
@@ -47,41 +47,3 @@ def get_points(user_a, user_b):
             return 0.0, 0
 
 
-# def get_points(user_a, user_b):
-#     a_answers = UserAnswer.objects.filter(user=user_a)
-#     b_answers = UserAnswer.objects.filter(user=user_b)
-#     a_total_awarded = 0
-#     a_points_possible = 0
-#     num_question = 0
-#     for a in a_answers:
-#         print('a:', a)
-#         print('b_answers:', b_answers)
-#         for b in b_answers:
-#             print('b:', b)
-#             if a.question.id == b.question.id:
-#                 num_question += 1
-#                 a_pref = a.other_user_answer
-#                 b_answer = b.user_answer
-#                 if a_pref == b_answer:
-#                     '''
-#                     a give point to b according to the importance level of a
-#                     '''
-#                     a_total_awarded += a.other_user_points
-#                 '''
-#                 assign total points
-#                 '''
-#                 a_points_possible += a.other_user_points
-#             print('{} give {} point of {} to {} '.format(user_a, a_total_awarded, a_points_possible, user_b))
-#     percent = a_total_awarded / Decimal(a_points_possible)
-#     print(percent, num_question)
-#     if percent == 0:
-#         percent = 0.000001
-#     return percent, num_question
-
-
-def get_users_match(user_a, user_b):
-    a = get_points(user_a, user_b)
-    b = get_points(user_b, user_a)
-    number_of_question = b[1]
-    match_decimal = (Decimal(a[0] * Decimal(b[0])) ** (1/Decimal(b[1])))
-    return match_decimal, number_of_question
