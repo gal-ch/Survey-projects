64c1fd823a5785c499a642deb297245319ae039b
gal hadad <galchadad@gmail.com>
2020-04-13 20:59:16 +0300

    update

--- matches/models.py
+++ matches/models.py
@@ -1,5 +1,9 @@
+import datetime
+
 from django.db import models
 from django.conf import settings
+from django.utils import timezone
+
 from .utils import get_users_match
 
 
@@ -14,18 +18,25 @@ class MatchManager(models.Manager):
         except:
             obj_2 = None
         if obj_1 and not obj_2:
+            obj_1.check_update()
             return obj_1, False
         elif obj_2 and not obj_1:
+            obj_2.check_update()
             return obj_2, False
         else:
             new_instance = self.create(user_a=user_a, user_b=user_b)
-            match_decimal, questions_answered = get_users_match(user_a, user_b)
-            new_instance.match_decimal = match_decimal
-            new_instance.questions_answered = questions_answered
-            new_instance.save()
+            new_instance.do_match()
             return new_instance, True
 
-            # need to: add match && save new_instance && return new_instance, True
+    def update_all(self):
+        queryset = self.all()
+        now = timezone.now()
+        offset1 = now - datetime.timedelta(hours=12)
+        offset2 = now - datetime.timedelta(hours=36)
+        queryset.filter(updated__gt=offset2).filter(updated__lte=offset1)
+        if queryset.count > 0:
+            for i in queryset:
+                i.check_update()
 
 
 class Match(models.Model):
@@ -37,5 +48,24 @@ class Match(models.Model):
     updated = models.DateTimeField(auto_now_add=False, auto_now=True)
 
     objects = MatchManager()
+
+    def do_match(self):
+        user_a = self.user_a
+        user_b = self.user_b
+        match_decimal, questions_answered = get_users_match(user_a, user_b)
+        self.match_decimal = match_decimal
+        self.questions_answered = questions_answered
+        self.save()
+
+    def check_update(self):
+        now = timezone.now()
+        offset = now - datetime.timedelta(hours=12)
+        if self.updated <= offset:
+            self.do_match()
+        else:
+            print('already updated')
+
+
+
 
 
